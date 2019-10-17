26a02a872ece9e1feec6724e55f0ab9f8f011492
Michael Carroll <carrollm2@gmail.com>
2019-10-17 01:48:26 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -25,7 +25,16 @@ def long_planeteer_calls(calls)
   return false
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(snacks)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+
+  cheese_types.each do |snack|
+    if snacks.include? snacks
+      return true
+    end
+  end
+
+  return nil
+
 end
