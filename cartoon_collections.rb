e5cbb9c8ed14a13c2b830e6922b413d6c4c35a01
Michael Carroll <carrollm2@gmail.com>
2019-10-17 01:33:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,7 @@
 def roll_call_dwarves(dwarves)
-  counter = 1
-  dwarves.each_with_index do |dwarf, index + 1|
-    puts "#{index}. #{dwarf}"
+
+  dwarves.each_with_index do |dwarf, index|
+    puts "#{index + 1}. #{dwarf}"
   end
 end
 
