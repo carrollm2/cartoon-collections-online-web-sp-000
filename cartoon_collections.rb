676178709acbeaa025e19c29ed695c5cc4522b1b
Michael Carroll <carrollm2@gmail.com>
2019-10-17 01:43:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,8 +16,12 @@ def summon_captain_planet(calls)
 
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.each do |call|
+    if call.length > 4
+      return true
+    end
+  return false
 end
 
 def find_the_cheese# code an argument here
