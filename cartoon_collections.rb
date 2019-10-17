ef6b169ba1240c0063342dbd00c455f7890a68b4
Michael Carroll <carrollm2@gmail.com>
2019-10-17 01:59:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -26,7 +26,7 @@ def long_planeteer_calls(calls)
 end
 
 def find_the_cheese(snacks)
-  # the array below is here to help
+
   cheese_types = ["cheddar", "gouda", "camembert"]
 
   snacks.each do |snack|
